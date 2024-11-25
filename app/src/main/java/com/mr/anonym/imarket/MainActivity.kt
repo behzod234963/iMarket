742c3afc6d1986c8a2bb6a7e1d2314e0e9ba5a18
Behzod <behzod96447@internet.ru>
2024-11-25 16:15:12 +0500

    first commit

---
+++ app/src/main/java/com/mr/anonym/imarket/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.mr.anonym.imarket
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mr.anonym.imarket.ui.theme.IMarketTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            IMarketTheme {
+
+            }
+        }
+    }
+}
